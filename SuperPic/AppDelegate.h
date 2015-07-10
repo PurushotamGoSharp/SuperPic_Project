4be02b86c0dcdb32fa710655780a0a10733f5668
ZT <zhanggs0712@163.com>
2015-07-10 23:27:25 +0800

    Initial Commit

---
+++ SuperPic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperPic
+//
+//  Created by iT on 7/10/15.
+//  Copyright (c) 2015 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
